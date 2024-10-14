614a652d0f5cda29e82e643dea24f3d023975799
reckless <biruktesfa444@gmail.com>
2024-10-14 18:38:43 +0300

    setting up the Row componenet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Raw from './Rows.jsx'
+import requests from './requests.js';
 
 // 27ab8be554458dc2e8d27a5f378e0908 api key
 ///base:https://api.themoviedb.org/3/movie/550
@@ -7,8 +8,19 @@ import Raw from './Rows.jsx'
 function App() {
   return (
     <div className="App">
-      <Raw title/>
-      <Raw title/>
+      <Raw 
+      title = "NERFLIX ORiGINAL" 
+      fetchiURL = {requests.fetchNetflixOriginal}
+      isLargeRow ={true}
+      />
+      <Raw title = "Trending Now" fetchiURL = {requests.fetchTrending}/>
+      <Raw title = "Top Rated"fetchiURL = {requests.fetchTopRated}/>
+      <Raw title = "Action Movies"fetchiURL = {requests.fetchActionMovies}/>
+      <Raw title = "Comedy Movies"fetchiURL = {requests.fetchComedyMovies}/>
+      <Raw title = "Horror Movies"fetchiURL = {requests.fetchHorrorMovies}/>
+      <Raw title = "Romance Movies"fetchiURL = {requests.fetchRomanceMovies}/>
+      <Raw title = "Documentaries"fetchiURL = {requests.fetchDocumentaries}/>
+      
     </div>
   );
 }

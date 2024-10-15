27ec997f22109c738e952e9ac650909823697b7f
reckless <biruktesfa444@gmail.com>
2024-10-15 12:44:03 +0300

    hanlding and passing trailer when a poster is touched

--- src/Rows.jsx
+++ src/Rows.jsx
@@ -1,38 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import axios from './axios'
-import "./raw.css"
+import React, { useEffect, useState } from 'react';
+import axios from './axios';
+import "./raw.css";
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title,fetchiURL,isLargeRow}) {
-
-  const[movies,setMovies] = useState([])
+function Row({ title, fetchiURL, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerURL, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchiURL);
-      console.log(request) 
-      setMovies(request.data.results)
+      console.log(request); 
+      setMovies(request.data.results);
       return request;
     }
     fetchData();
-  },[fetchiURL])
+  }, [fetchiURL]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerURL) {
+      setTrailerUrl(''); // Close the trailer if it's already open
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v")); // Get the video ID
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
   return (
     <div className='row'>
       <h2>{title}</h2>
       <div className="row__posters">
-      {movies.map((movie) => (
+        {movies.map((movie) => (
           <img
-          key={movie.id}
+            key={movie.id}
+            onClick={() => handleClick(movie)} // Handle click to play trailer
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${base_url}${isLargeRow?movie.poster_path:movie.backdrop_path}`} 
+            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
             alt={movie.name} 
           />
         ))}
       </div>
+      {trailerURL && <YouTube videoId={trailerURL} opts={opts} />}
     </div>
-  )
+  );
 }
 
 export default Row;
